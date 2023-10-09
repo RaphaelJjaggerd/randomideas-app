c76764036a53e5ea174f57a8dfc8aaf7ac6f2f4e
Raphael <rjjaggerd@gmail.com>
2023-10-09 23:45:04 +0300

    Streamlined routes

--- routes/ideas.js
+++ routes/ideas.js
@@ -96,22 +96,4 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-router.deletetemp('/:id', async (req, res) => {
-  try {
-    await Idea.findByIdAndDelete(req.params.id);
-    res.json({ success: true, data: {} });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, error: 'Something Went Wrong' });
-  }
-});
-router.deletetemp2('/:id', async (req, res) => {
-  try {
-    await Idea.findByIdAndDelete(req.params.id);
-    res.json({ success: true, data: {} });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, error: 'Something Went Wrong' });
-  }
-});
 module.exports = router;
