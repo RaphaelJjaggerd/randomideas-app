a81483d2f849ac3ce0b79b29083e3cff613411a9
Raphael <rjjaggerd@gmail.com>
2023-10-14 17:09:13 +0300

    New schema

--- models/Idea.js
+++ models/Idea.js
@@ -33,4 +33,21 @@ const Ideachema = new mongoose.Schema({
   },
 });
 
+const Ideacha = new mongoose.Schema({
+  text: {
+    type: String,
+    required: [true, 'Please add a text field'],
+  },
+  tag: {
+    type: String,
+  },
+  username: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
 module.exports = mongoose.model('Idea', IdeaSchema);
